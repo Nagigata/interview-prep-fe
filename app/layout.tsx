7e2bcb72f74f1c2ba1a00e26ba36b177e297b289
Hoàng Đăng Xuân Thiện <129950033+Nagigata@users.noreply.github.com>
2026-03-25 19:38:55 +0700

    Setup app and Auth Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import { Mona_Sans, Geist } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Toaster } from "sonner";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Mona_Sans({
   variable: "--font-mona-sans",
@@ -12,7 +13,8 @@ const geistSans = Mona_Sans({
 
 export const metadata: Metadata = {
   title: "PrepWise",
-  description: "AI-powered mock interview platform for software engineers to practice and improve their coding interview skills.",
+  description:
+    "AI-powered mock interview platform for software engineers to practice and improve their coding interview skills.",
 };
 
 export default function RootLayout({
@@ -23,9 +25,19 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("h-full", "antialiased", "dark", geistSans.variable, "font-sans", geist.variable)}
+      className={cn(
+        "h-full",
+        "antialiased",
+        "dark",
+        geistSans.variable,
+        "font-sans",
+        geist.variable,
+      )}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col pattern">
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
